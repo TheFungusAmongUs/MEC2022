f87e8b1f6c6e371315d742b9a142a18d892c2003
Yonge Bai <97137323+YongeBai@users.noreply.github.com>
2022-11-12 16:30:06 -0500

    Update engcomp.py

--- engcomp.py
+++ engcomp.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import sklearn 
 import pickle
-import numpy
+
 
 class Predictor():
   def __init__(self):
@@ -10,6 +10,7 @@ class Predictor():
   def processData(self, input_data):
     data = pd.read_csv(input_data)
     data = data.dropna(axis=1).reset_index(drop=True)
+    data.drop(columns=data.columns[0], axis=1, inplace=True)
     return data
 
   def predict(self, input_data):
